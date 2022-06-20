f2d7ab8ada47d51d52bfb3f6dbcd24541244d70f
tugalsan karabacak <tugalsan@gmail.com>
2022-06-20 23:07:36 +0300

    Initial commit

---
+++ src/main/java/com/tugalsan/api/coronator/client/TGS_Coronator.java
@@ -0,0 +1,73 @@
+package com.tugalsan.api.coronator.client;
+
+import com.tugalsan.api.compiler.client.*;
+import com.tugalsan.api.pack.client.*;
+import com.tugalsan.api.validator.client.*;
+import java.util.*;
+
+public class TGS_Coronator<T> {
+
+    //CONSTRUCTOR
+    private T bufferedValue;
+
+    public TGS_Coronator(T initVal) {
+        bufferedValue = initVal;
+    }
+
+    public static <T> TGS_Coronator of(T initialValue) {
+        return new TGS_Coronator(initialValue);
+    }
+
+    //LOADERS
+    private List<TGS_Pack3<TGS_CompilerType1<T, T>, TGS_ValidatorType1, /*is it stopper*/ Boolean>> pack = new ArrayList();
+
+    public TGS_Coronator<T> anoint(TGS_CompilerType1<T, T> val) {
+        pack.add(new TGS_Pack3(val, null, null));
+        return this;
+    }
+
+    public TGS_Coronator<T> coronateIf(TGS_ValidatorType1<T> validate, TGS_CompilerType1<T, T> val) {
+        if (validate.validate(bufferedValue)) {
+            pack.add(new TGS_Pack3(null, validate, true));
+        }
+        return this;
+    }
+
+    public TGS_Coronator<T> anointIf(TGS_ValidatorType1<T> validate, TGS_CompilerType1<T, T> val) {
+        if (validate.validate(bufferedValue)) {
+            pack.add(new TGS_Pack3(val, validate, false));
+        }
+        return this;
+    }
+
+    public TGS_Coronator<T> anointAndCoronateIf(TGS_ValidatorType1<T> validate, TGS_CompilerType1<T, T> val) {
+        if (validate.validate(bufferedValue)) {
+            pack.add(new TGS_Pack3(val, validate, true));
+        }
+        return this;
+    }
+
+    //FETCHER
+    public T coronate() {
+        for (var comp : pack) {
+            var setter = comp.value0;
+            var validator = comp.value1;
+            var validatorIsStopper = comp.value2;
+            if (validator == null) {
+                bufferedValue = setter.compile(bufferedValue);
+                continue;
+            }
+            if (!validator.validate(bufferedValue)) {
+                continue;
+            }
+            if (setter != null) {
+                bufferedValue = setter.compile(bufferedValue);
+            }
+            if (validatorIsStopper) {
+                return bufferedValue;
+            }
+        }
+        return bufferedValue;
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.tugalsan.api.coronator {
+    requires com.tugalsan.api.compiler;
+    requires com.tugalsan.api.executable;
+    requires com.tugalsan.api.validator;
+    requires com.tugalsan.api.pack;
+    exports com.tugalsan.api.coronator.client;
+}
